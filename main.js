b7327be3abba3ae4a805f5f81cf22087caf36718
void-memories <voidmemories@Namans-MacBook-Air.local>
2021-07-04 02:43:04 +0530

    dynamic nav

--- main.js
+++ main.js
@@ -105,3 +105,24 @@ $(window).scroll(function(){
         .end().filter("[href=#"+id+"]").parent().addClass("active");
   }                   
 });
+
+
+
+
+
+
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 100) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('#global-nav').removeClass('scrolled-nav');
+    } 
+    
+  }); 
+  
+});
